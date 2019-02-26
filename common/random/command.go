96b3d91a031f64d69ff1ba692e62d5efcd71df2f
witooh <witoo@plimble.com>
2019-02-26 15:17:36 +0700

    add random

---
+++ common/random/command.go
@@ -0,0 +1,82 @@
+package random
+
+import (
+	"encoding/json"
+	"strings"
+
+	random "github.com/Pallinder/go-randomdata"
+	"github.com/onedaycat/zamus/command"
+	"github.com/onedaycat/zamus/invoke"
+)
+
+type commandBuilder struct {
+	cmd *command.Command
+}
+
+func Command() *commandBuilder {
+	username := random.Noun()
+	email := random.Email()
+	ip := random.IpV4Address()
+	return &commandBuilder{
+		cmd: &command.Command{
+			Function: random.SillyName(),
+			Identity: &invoke.Identity{
+				Sub:      username,
+				SourceIP: []string{ip},
+				Groups:   []string{"user"},
+				Username: username,
+				Claims: invoke.Claims{
+					Email:       email,
+					Permissions: make(invoke.Permissions),
+				},
+			},
+		},
+	}
+}
+
+func (b *commandBuilder) Function(fn string) *commandBuilder {
+	b.cmd.Function = fn
+
+	return b
+}
+
+func (b *commandBuilder) Arg(v interface{}) *commandBuilder {
+	data, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+
+	b.cmd.Args = data
+
+	return b
+}
+
+func (b *commandBuilder) Source(v interface{}) *commandBuilder {
+	data, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+
+	b.cmd.Source = data
+
+	return b
+}
+
+func (b *commandBuilder) ValidPermission(key string, permissions ...string) *commandBuilder {
+	b.cmd.PermissionKey = key
+	b.cmd.Identity.Claims.Permissions[key] = strings.Join(permissions, ",")
+
+	return b
+}
+
+func (b *commandBuilder) InvalidPermission() *commandBuilder {
+	pem := random.SillyName()
+	b.cmd.PermissionKey = pem
+	b.cmd.Identity.Claims.Permissions[pem] = strings.Join([]string{random.SillyName(), random.SillyName()}, ",")
+
+	return b
+}
+
+func (b *commandBuilder) Build() *command.Command {
+	return b.cmd
+}
---
+++ common/random/event.go
@@ -0,0 +1,74 @@
+package random
+
+import (
+	"encoding/json"
+	"time"
+
+	random "github.com/Pallinder/go-randomdata"
+	"github.com/onedaycat/gocqrs/common/eid"
+	"github.com/onedaycat/zamus/eventstore"
+)
+
+type eventBuilder struct {
+	msg *eventstore.EventMsg
+}
+
+func EventMsg() *eventBuilder {
+	aggid := eid.GenerateID()
+	seq := int64(random.Number(1, 10))
+	t := time.Now().Unix()
+	eventID := eid.CreateEventID(aggid, seq)
+
+	return &eventBuilder{
+		msg: &eventstore.EventMsg{
+			EventID:      eventID,
+			EventType:    random.SillyName(),
+			EventVersion: "1",
+			AggregateID:  aggid,
+			Time:         t,
+			Seq:          seq,
+			TimeSeq:      eventstore.TimeSeq(t, seq),
+		},
+	}
+}
+
+func (b *eventBuilder) Build() *eventstore.EventMsg {
+	return b.msg
+}
+
+func (b *eventBuilder) EventType(eventType string) *eventBuilder {
+	b.msg.EventType = eventType
+
+	return b
+}
+
+func (b *eventBuilder) AggregateID(aggid string) *eventBuilder {
+	b.msg.AggregateID = aggid
+
+	return b
+}
+
+func (b *eventBuilder) NewEvent() *eventBuilder {
+	b.msg.Seq = 0
+	b.msg.TimeSeq = eventstore.TimeSeq(b.msg.Time, 0)
+	b.msg.EventID = eid.CreateEventID(b.msg.AggregateID, 0)
+
+	return b
+}
+
+func (b *eventBuilder) Metadata(metadata *eventstore.Metadata) *eventBuilder {
+	b.msg.Metadata, _ = metadata.Marshal()
+
+	return b
+}
+
+func (b *eventBuilder) Events(event interface{}) *eventBuilder {
+	data, err := json.Marshal(event)
+	if err != nil {
+		panic(err)
+	}
+
+	b.msg.Event = data
+
+	return b
+}
