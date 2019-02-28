730ac9e9dab87f53c33d50980c89f122c3ffc0bc
witooh <witoo@plimble.com>
2019-02-28 18:22:26 +0700

    add set

---
+++ common/set.go
@@ -0,0 +1,27 @@
+package common
+
+type Set map[string]struct{}
+
+func NewSet() Set {
+	return make(map[string]struct{}, 100)
+}
+
+func (s Set) Set(key string) {
+	s[key] = struct{}{}
+}
+
+func (s Set) Has(key string) bool {
+	_, ok := s[key]
+	return ok
+}
+
+func (s Set) List(key string) []string {
+	i := 0
+	keys := make([]string, len(s))
+	for key := range s {
+		keys[i] = key
+		i++
+	}
+
+	return keys
+}
