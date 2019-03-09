74da91396741a499017f53e328c35e450067a28f
witooh <witoo@plimble.com>
2019-03-09 12:38:05 +0700

    update

--- invoke/invoker.go
+++ invoke/invoker.go
@@ -29,11 +29,14 @@ type Invoker interface {
 // {"errorMessage":"Account_AccountCreated: Account already created","errorType":"AppError"}
 type InvokeErrorPayload struct {
 	ErrorMessage string `json:"errorMessage"`
-	ErrorType    string `json:"errorType"`
 }
 
-func UnmarshalInvokeErrorPayload(payload []byte) (*InvokeErrorPayload, error) {
+func UnmarshalInvokeErrorPayload(payload []byte) errors.Error {
 	in := &InvokeErrorPayload{}
 	err := json.Unmarshal(payload, in)
-	return in, errors.Wrap(err)
+	if err != nil {
+		return errors.Wrap(err)
+	}
+
+	return errors.ParseError(in.ErrorMessage)
 }
