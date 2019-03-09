7a6ffa8cdc8af3c9df0108d692b206543e45fd0d
witooh <witoo@plimble.com>
2019-03-09 12:14:41 +0700

    update

--- invoke/invoker.go
+++ invoke/invoker.go
@@ -2,6 +2,9 @@ package invoke
 
 import (
 	"context"
+	"encoding/json"
+
+	"github.com/onedaycat/errors"
 
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/lambda"
@@ -13,10 +16,24 @@ type InvokeOption = request.Option
 type InvokeAsyncInput = lambda.InvokeAsyncInput
 type InvokeAsyncOutput = lambda.InvokeAsyncOutput
 
+var LATEST = "$LATEST"
+
 //go:generate mockery -name=Invoker
 type Invoker interface {
 	Invoke(input *InvokeInput) (*InvokeOutput, error)
 	InvokeWithContext(ctx context.Context, input *InvokeInput, opts ...InvokeOption) (*InvokeOutput, error)
 	InvokeAsync(input *InvokeAsyncInput) (*InvokeAsyncOutput, error)
 	InvokeAsyncWithContext(ctx context.Context, input *InvokeAsyncInput, opts ...InvokeOption) (*InvokeAsyncOutput, error)
+}
+
+// {"errorMessage":"Account_AccountCreated: Account already created","errorType":"AppError"}
+type InvokeErrorPayload struct {
+	ErrorMessage string `json:"errorMessage"`
+	ErrorType    string `json:"errorType"`
+}
+
+func UnmarshalInvokeErrorPayload(payload []byte) (*InvokeErrorPayload, error) {
+	in := &InvokeErrorPayload{}
+	err := json.Unmarshal(payload, in)
+	return in, errors.Wrap(err)
 }
