b6f76f71babc6aa8fa4a8808f890348feb1e9374
witooh <witoo@plimble.com>
2019-03-28 13:12:39 +0700

    update

--- trigger/errors.go
+++ trigger/errors.go
@@ -3,17 +3,18 @@ package trigger
 import "github.com/onedaycat/zamus/errors"
 
 var (
-	ErrInternalError    = errors.ErrInternalError
-	ErrInvalidRequest   = errors.ErrInvalidRequest
-	ErrValidateError    = errors.ErrValidateError
-	ErrPermissionDenied = errors.ErrPermissionDenied
-	ErrTimeout          = errors.ErrTimeout
-	ErrUnauthorized     = errors.ErrUnauthorized
-	ErrUnavailable      = errors.ErrUnavailable
-	ErrNotImplement     = errors.ErrNotImplement
-	ErrUnableUnmarshal  = errors.ErrUnableUnmarshal
-	ErrUnableMarshal    = errors.ErrUnableMarshal
-	ErrUnableEncode     = errors.ErrUnableEncode
-	ErrUnableDecode     = errors.ErrUnableDecode
-	ErrUnableApplyEvent = errors.ErrUnableApplyEvent
+	ErrInternalError      = errors.ErrInternalError
+	ErrInvalidRequest     = errors.ErrInvalidRequest
+	ErrValidateError      = errors.ErrValidateError
+	ErrPermissionDenied   = errors.ErrPermissionDenied
+	ErrTimeout            = errors.ErrTimeout
+	ErrUnauthorized       = errors.ErrUnauthorized
+	ErrUnavailable        = errors.ErrUnavailable
+	ErrNotImplement       = errors.ErrNotImplement
+	ErrUnableUnmarshal    = errors.ErrUnableUnmarshal
+	ErrUnableMarshal      = errors.ErrUnableMarshal
+	ErrUnableEncode       = errors.ErrUnableEncode
+	ErrUnableDecode       = errors.ErrUnableDecode
+	ErrUnableApplyEvent   = errors.ErrUnableApplyEvent
+	ErrUnablePublishEvent = errors.ErrUnablePublishEvent
 )
