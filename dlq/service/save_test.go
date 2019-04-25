16bff20711a13d37acd47befdff1333806483fd1
witooh <witoo@plimble.com>
2019-04-25 13:44:20 +0700

    update

---
+++ dlq/service/save_test.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"github.com/onedaycat/zamus/dlq"
+	"github.com/onedaycat/zamus/dlq/mocks"
+	"github.com/onedaycat/zamus/service"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSaveSuccess(t *testing.T) {
+	storage := &mocks.Storage{}
+
+	h := NewHandler(storage)
+
+	msg := &dlq.DLQMsg{
+		ID:             "123",
+		Service:        "hello",
+		Time:           111,
+		Version:        "1.0.0",
+		LambdaFunction: "sel-hello-dev-srv",
+		EventMsgs:      []byte(`hello`),
+		Errors:         nil,
+	}
+
+	input := &SaveDLQMsgInput{
+		Msg: msg,
+	}
+
+	req := service.NewRequest(SaveDLQMsgMethod).WithInput(input)
+	storage.On("Save", mock.Anything, msg).Return(nil)
+
+	res, err := h.SaveDLQMsg(context.Background(), req)
+	require.NoError(t, err)
+	require.Equal(t, "123", res)
+}
---
+++ dlq/storage/invoke/storage_test.go
@@ -0,0 +1,47 @@
+package invoke
+
+import (
+	"context"
+	"testing"
+
+	"github.com/onedaycat/zamus/dlq"
+	"github.com/onedaycat/zamus/dlq/service"
+	"github.com/onedaycat/zamus/invoke"
+	"github.com/onedaycat/zamus/invoke/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCRUD(t *testing.T) {
+	invoker := &mocks.Invoker{}
+	s := New(invoker, "arn")
+
+	msg := &dlq.DLQMsg{
+		ID:             "123",
+		Service:        "hello",
+		Time:           111,
+		Version:        "1.0.0",
+		LambdaFunction: "sel-hello-dev-srv",
+		EventMsgs:      []byte(`hello`),
+		Errors:         nil,
+	}
+
+	input := &service.SaveDLQMsgInput{
+		Msg: msg,
+	}
+
+	req := invoke.NewRequest(service.SaveDLQMsgMethod).WithInput(input)
+	invoker.On("Invoke", mock.Anything, mock.Anything, req, nil).Return(nil)
+
+	err := s.Save(context.Background(), &dlq.DLQMsg{
+		ID:             "123",
+		Service:        "hello",
+		Time:           111,
+		Version:        "1.0.0",
+		LambdaFunction: "sel-hello-dev-srv",
+		EventMsgs:      []byte(`hello`),
+		Errors:         nil,
+	})
+
+	require.NoError(t, err)
+}
