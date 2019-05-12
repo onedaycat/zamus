a26030d001b54c3c17df915ebb8e7740337781cf
witooh <witoo@plimble.com>
2019-05-13 00:26:20 +0700

    update

--- makefile.go
+++ makefile.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Release() {
-    ver := "v0.81.0"
+    ver := "v0.82.0"
     mg.BuildLinux("./setup/eventsource", "./setup/eventsource/bin/app")
     mg.BuildLinux("./setup/dlq", "./setup/dlq/bin/app")
     mg.ExecX("zip ./eventsource.zip app").Dir("./setup/eventsource/bin").Run()
